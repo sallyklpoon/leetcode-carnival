218d27ab69a8637c20eb801b435043c79da7d1fc
Sally Poon <sallypoon@hotmail.com>
2022-09-07 12:18:35 -0700

    update v2

--- LeetCode/Easy/1_two_sum/1_two_sum.py
+++ LeetCode/Easy/1_two_sum/1_two_sum.py
@@ -1,10 +1,10 @@
-def two_sum_bf(nums: list, target:int) -> list:
+def two_sum_bf(nums:list, target:int) -> list:
     """
     Find the index of two int resulting in the target sum.
 
     This solution is considered brute force as there are two implicit loops nested within each other.
     While-loop runs at O(N-1) at worse case and the Python *in* operator conducts a list search,
-    which should be O(N) worse case.
+    which should be O(N) worse case
     List append at O(1)
 
     :param nums: a list of int
@@ -16,23 +16,26 @@ def two_sum_bf(nums: list, target:int) -> list:
     while len(output) == 0:
         difference = target - nums[index]
         if difference in nums[index + 1:]:
-            output.append(index)
-            output.append(nums[index + 1:].index(difference) + index + 1)
+            return [index, nums[index + 1:].index(difference) + index + 1]
         index += 1
-    return output
 
+ def two_sum_v2(nums:list, target:int) -> list:
+     """
+     Find the index of two int resulting in the target sum
 
-def two_sum_v2(nums: list, target:int) -> list:
-    """ WIP -- incomplete solution atm, testing edge cases.
+     This solution will work at O(N) time. It goes through the list of nums once, storing the index
+     and value in the hashmap, and checks if its difference addend exists already in the hashmap keys.
+     Finding a hash key is at O(1) time
 
-    :param nums:
-    :param target:
-    :return:
-    """
-    differences = [target - num for num in nums]    # O(N)
-    possible_addends = list(set(nums).intersection(differences))
-    return [nums.index(possible_addends[0]), nums.index(possible_addends[1])]
-
+    :param nums: a list of int
+    :param target: an int, the target sum
+    :return: a list of int, indexes of the sum nums
+     """
+     i_map = {}
+     for i, num in enumerate(nums):
+         if target - num in i_map.keys():
+             return [i, i_map[target - num]]
+         i_map[num] = i
 
 
 if __name__ == '__main__':

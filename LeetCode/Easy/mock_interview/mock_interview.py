e56069b06b2388d245da906a502736ed0314449f
Sally Poon <sallypoon@hotmail.com>
2022-09-16 23:17:40 -0700

    mock interview answer

---
+++ LeetCode/Easy/mock_interview/mock_interview.py
@@ -0,0 +1,34 @@
+"""
+Mock Interview Sept 16, 2022
+"""
+
+
+def num_not_in_list(nums: list) -> list:
+    """
+    Return the numbers not in a list that should contain list length as range.
+
+    :param nums: list of int
+    :return: list of int
+
+    >>> num_not_in_list([])
+    []
+    >>> num_not_in_list([1])
+    []
+    >>> num_not_in_list([3, 1, 2])
+    []
+    >>> num_not_in_list([4, 4, 2, 5, 4])
+    [1, 3]
+    """
+    upper = len(nums)
+    num_tracker = [None for i in range(upper + 1)]
+    output = []
+
+    for i in range(upper):
+        num_tracker[nums[i]] = True
+
+    for j in range(1, upper + 1):
+        if not num_tracker[j]:
+            output.append(j)
+
+    return output
+

4a12990fcda9e492761128830ee0e490da081958
Sally Poon <sallypoon@hotmail.com>
2022-09-22 14:36:36 -0700

    bf solution with 3 loops

--- LeetCode/Easy/724_pivot_index/724_pivot_index.py
+++ LeetCode/Easy/724_pivot_index/724_pivot_index.py
@@ -11,21 +11,31 @@ left = left side sum
 
 [1,7,3,6,5,6]
 
+[28, 27, 20, 17, 11, 6]
 [1, 8, 11, 17, 22, 28]
----------------^
+--------^-------^
 
 1: 1 vs 11-8 = 3 -- x
 2: 8 vs 17 - 11 = 6 -- x
 3: 11 vs 22 - 17 = 5 -- x
+
+[2, -1, 1]
+
+[2, 1, 2]
+[2  0 , 1]
 
 """
 
 
 def pivot_index_bf(nums: list) -> int:
     """
+    Return the pivot index of a list of int.
 
-    :param nums:
-    :return:
+    Time Complexity: O(N) - pass 3 times
+    Space Complexity: O(N) - stores an extra list
+
+    :param nums: a list of int
+    :return: an int, the pivot index
 
     >>> pivot_index_bf([1, 7, 3, 6, 5, 6])
     3
@@ -33,18 +43,21 @@ def pivot_index_bf(nums: list) -> int:
     -1
     >>> pivot_index_bf([2, 1, -1])
     0
+    >>> pivot_index_bf([-1,-1,0,1,0,-1])
+    4
     """
     added_bw = [num for num in nums]
 
-    # go forward and sum everything
-    for i in range(1, len(added_bw)):
-        added_bw[i] += added_bw[i - 1]
+    # go backwards, sum everything
+    for i in range(-2, -(len(added_bw) + 1), -1):
+        added_bw[i] += added_bw[i + 1]
 
-    # go backward and sum everything + compare
-    for i in range(2, len(nums) - 1):
-        if nums[-i] + nums[-i + 1] == added[-i - 2]:
-            return len(nums) - i - 1
-        else:
-            nums[-i] += nums[-i + 1]
+    # go forward sum everything + compare
+    for j in range(1, len(nums)):
+        nums[j] += nums[j - 1]
 
-    return 0 if nums[1] + nums[2] == 0 else -1
+    for k in range(len(nums)):
+        if added_bw[k] == nums[k]:
+            return k
+
+    return -1

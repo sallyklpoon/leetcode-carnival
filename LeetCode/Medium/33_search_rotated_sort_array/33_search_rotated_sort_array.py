5bc01869138d2180f13b43d7de5c49825fa8a20b
Sally Poon <sallypoon@hotmail.com>
2022-11-30 13:46:28 -0800

    add time and space complexity

--- LeetCode/Medium/33_search_rotated_sort_array/33_search_rotated_sort_array.py
+++ LeetCode/Medium/33_search_rotated_sort_array/33_search_rotated_sort_array.py
@@ -7,6 +7,9 @@ def search(nums: list, target: int) -> int:
     """
     Find the index of a target integer in a rotated, sorted array in O(log n) time.
 
+    Time: O(log n) , binary search
+    Space: O(1), store l and r
+
     :param nums: a list of int
     :param target: an int
     :return: an int, the index of the target or -1 if not found

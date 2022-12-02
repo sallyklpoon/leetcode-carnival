4d4ebd8c84acd23fdc74ff281d004fe07517e2ee
Sally Poon <sallypoon@hotmail.com>
2022-12-01 16:57:12 -0800

    add stack solutions

--- LeetCode/Medium/1209_remove_adjacent_duplicates_II/1209_remove_adjacent_duplicates_II.py
+++ LeetCode/Medium/1209_remove_adjacent_duplicates_II/1209_remove_adjacent_duplicates_II.py
@@ -14,14 +14,90 @@ str = s[:start_i] + s[start+i + count:] -> dbbcccbdaa
 
 for every iteration of remove duplicates, we will keep iterating until
 
-===== Stack Strat
-stack = [de]
+===== Stack Strat (naive)
+stack = [ddbbbdaa]
 
+count = 1
 if curr != stack top:
    add to stack
-if equal to stack
+   count = 1 <-- reset to 1
+if equal to stack top:
+   count += 1
+if count == k:
+   stack.pop()
 
+if len(stack) != s_original:
+   repeat the above steps
+
+set s = stack string
+
+check  if changed, if it's changed from original string length, we will keep removing
+
+
+[ddbbbdaa]
+c = 2
+k = 3
 """
+
+
+def remove_duplicates_stack(s: str, k: int) -> str:
+    """
+    Remove duplicates of k-length until none, O(N), optimized to use stack of tuples.
+    Teach tuple represents the previous character and the consecutive count.
+
+    Time: O(N)
+    Space: O(N)
+
+    :param s: a string
+    :param k: an integer
+    :return: a string
+    """
+    stack = []
+    for char in s:
+        if not stack or char != stack[-1][0]:
+            stack.append((char, 1))
+        else:
+            if char == stack[-1][0]:
+                stack[-1] = (char, stack[-1][1] + 1)
+                if stack[-1][1] == k:
+                    stack.pop()
+    return ''.join([char[0] * char[1] for char in stack])
+
+
+def remove_duplicates_stack_naive(s: str, k: int) -> str:
+    """
+    Remove duplicates of k-length until none.
+
+    Time = O(n^2) worst time, as it reiterates through each time there's a change
+    Space = O(n)
+
+    This solution has good space complexity, but poor time complexity
+
+    :param s: a string
+    :param k: an int
+    :return: a string
+    """
+    changed = True
+    while changed:
+        stack = []
+        adj_count = 1
+        for char in s:
+            if not stack:
+                stack.append(char)
+                continue
+            else:
+                if char == stack[-1]:
+                    adj_count += 1
+                    if adj_count == k:
+                        stack = stack[:-(k - 1)]
+                        adj_count = 1
+                        continue
+                elif char != stack[-1]:
+                    adj_count = 1
+                stack.append(char)
+        changed = not len(stack) == len(s)
+        s = stack
+    return ''.join(s)
 
 
 def remove_duplicates(s: str, k: int) -> str:

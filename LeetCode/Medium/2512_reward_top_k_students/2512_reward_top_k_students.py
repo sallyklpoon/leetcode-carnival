69791930001c7d57a8836746d658621a9dbaa7e5
Sally Poon <sallypoon@hotmail.com>
2023-04-19 23:29:28 -0700

    Add priority queue solution

--- LeetCode/Medium/2512_reward_top_k_students/2512_reward_top_k_students.py
+++ LeetCode/Medium/2512_reward_top_k_students/2512_reward_top_k_students.py
@@ -18,13 +18,13 @@ return top k
 - seems that any one neg word is sufficient for negative feedback
 """
 from typing import List
+from queue import PriorityQueue
 
 
 def top_students(positive_feedback: List[str], negative_feedback: List[str], report: List[str],
                  student_id: List[int], k: int) -> List[int]:
     n = len(student_id)
-    word_map = {word: 3 for word in positive_feedback}
-    word_map.update({word: -1 for word in negative_feedback})
+    word_map = {word: 3 for word in positive_feedback} | {word: -1 for word in negative_feedback}
     scores = []
 
     for i in range(n):
@@ -40,3 +40,27 @@ def top_students(positive_feedback: List[str], negative_feedback: List[str], rep
     for j in range(k):
         ans.append(scores[j][0])
     return ans
+
+
+def top_students_pq(positive_feedback: List[str], negative_feedback: List[str], report: List[str],
+                   student_id: List[int], k: int) -> List[int]:
+    n = len(student_id)
+    scores = PriorityQueue()
+    word_map = {word: 3 for word in positive_feedback} | {word: -1 for word in negative_feedback}
+
+    for i in range(n):
+        score = 0
+        for token in report[i].split(' '):
+            score += word_map.get(token, 0)
+        scores.put((-score, student_id[i]))
+        if scores.qsize() > k:
+            scores.get()
+
+    ans = []
+    for j in range(k):
+        ans.append(abs(scores.get()[1]))
+    return ans
+
+
+print(top_students_pq(positive_feedback=["smart", "brilliant", "studious"], negative_feedback=["not"],
+                     report=["this student is studious", "the student is smart"], student_id=[1, 2], k=2))
